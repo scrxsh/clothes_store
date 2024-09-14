62e963bf5d4b0d69d4524f330c5e2ac43f3d6471
miguel angel sierra  cruz <miguelsierra1512@gmail.com>
2024-09-14 17:19:17 -0500

    Delete tienda_ropa directory

--- tienda_ropa/src/main/java/com/example/tienda_ropa/repository/ProveedoresRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tienda_ropa.repository;
-
-import com.example.tienda_ropa.model.ProveedoresModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProveedoresRepository extends JpaRepository<ProveedoresModel,Long> {
-}
--- tienda_ropa/src/main/java/com/example/tienda_ropa/repository/VentasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tienda_ropa.repository;
-
-import com.example.tienda_ropa.model.VentasModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentasRepository extends JpaRepository<VentasModel,Long> {
-}
