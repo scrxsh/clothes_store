9d2aff2b01f115a12852f5b98e80cbfffd5008c3
Will ZP <107649183+scrxsh@users.noreply.github.com>
2024-10-15 10:10:06 -0500

    Update webConfig.java

--- back-end/src/main/java/com/example/tienda_ropa/configs/webConfig.java
+++ back-end/src/main/java/com/example/tienda_ropa/configs/webConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class webConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/images/**").addResourceLocations("file:uploads/images");
     }
 }
